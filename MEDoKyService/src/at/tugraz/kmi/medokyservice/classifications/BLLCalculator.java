ffc54cef7ee18a8c8a67965ec3ee562ee99b2794
Simone <simone.kopeinik@tugraz.at>
2014-09-09 16:13:49 +0200

    bll

--- MEDoKyService/src/at/tugraz/kmi/medokyservice/classifications/BLLCalculator.java
+++ MEDoKyService/src/at/tugraz/kmi/medokyservice/classifications/BLLCalculator.java
@@ -1,100 +1,139 @@
 package at.tugraz.kmi.medokyservice.classifications;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class BLLCalculator {
 	
-/*
-	public static List<Map<Integer, Double>> getArtifactMaps(List<Bookmark> userLines, List<Bookmark> testLines, 
-			List<Long> timestampList, List<Double> denomList, double dVal, boolean normalize) {
-		
-		List<Map<Integer, Double>> maps = new ArrayList<Map<Integer, Double>>();
-		for (Bookmark data : userLines) {
-			int userID = 0;
-			//System.out.println(data);
-			userID = data.getUserID();
-			
-			long baselineTimestamp = -1;
-			if (userID >= maps.size()) {
-				baselineTimestamp = Utilities.getBaselineTimestamp(testLines, userID, false);
-				
-				timestampList.add(baselineTimestamp);
-				if (baselineTimestamp != -1) {
-					maps.add(addActValue(data, new LinkedHashMap<Integer, Double>(), baselineTimestamp,  dVal));
-				} else {
-					maps.add(null);
-				}
-			} else {
-				baselineTimestamp = timestampList.get(userID);
-				if (baselineTimestamp != -1) {
-					addActValue(data, maps.get(userID), baselineTimestamp, dVal);
-				}
-			}
-		}
-		// normalize values
-		for (Map<Integer, Double> map : maps) {
-			double denom = 0.0;
-			if (map != null) {
-				for (Map.Entry<Integer, Double> entry : map.entrySet()) {
-					if (entry != null) {
-						double actVal = Math.log(entry.getValue());
-						denom += Math.exp(actVal);
-						entry.setValue(actVal);
-					}
-				}
-				denomList.add(denom);
-				if (normalize) {
-					for (Map.Entry<Integer, Double> entry : map.entrySet()) {
-						if (entry != null) {
-							double actVal = Math.exp(entry.getValue());
-							entry.setValue(actVal / denom);
-						}
-					}
-				}
-			}
-		}
-		
-		return maps;
-	}
-	*/
 	
-	public Map<Integer, String> getRankedTagList(int userID) {
-		Map<Integer, String> resultMap = new LinkedHashMap<Integer, String>();
-		Map<Integer, Double> actValues = new LinkedHashMap<Integer, Double>();
-		//TODO: some code here 
-		// call method to get TagsByGroup()
-		// call method to get TagsByUser()
-		// pre-process data 
+	public List<String> getRankedTagList(int userID) {
+		Map<String, Long> userTag = new LinkedHashMap<String, Long>();
+		Map<String, Integer> tagsByGroup = new LinkedHashMap<String, Integer>();
 		
-		//addActValue(actValues, time)
-		// normalize values
+		Map<String, Double> actValues = new LinkedHashMap<String, Double>();
+		Map<String, Double> mpValues = new LinkedHashMap<String, Double>();
+		List <String> recTags = new LinkedList<String>();
+		
+		
+		//getTagsByUser(userId):<tag, timestamp>
+		actValues = this.calculateNormalizedActValue(userTag);
+				
+		//getTagsByGroup(groupId):<tag, frequency>
+		mpValues = this.calculateNormalizedMostPopular(tagsByGroup);
+		
+		
 		// add MPValue to act values
+		for (Map.Entry<String, Double> entry: actValues.entrySet()){
+			if (mpValues.containsKey(entry.getKey()))
+				actValues.put(entry.getKey(), entry.getValue()+mpValues.get(entry.getKey()));
+		}
+		
+		
 		// determine 6 highest values
 		// write 6 values to result Map or set TagRecommendations
-		return resultMap;
+		
+		ActValueComparator actvc = new ActValueComparator(actValues);
+		TreeMap<String, Double> resultMap = new TreeMap<String, Double>(actvc); 
+		resultMap.putAll(actValues);
+		
+		int i=0;
+		for (Map.Entry<String, Double> entry: resultMap.entrySet()){
+			recTags.add(i, entry.getKey());
+			i++;
+			if (i==6)
+				break;
+		}
+		return recTags;
 	}
+
+
 	
-	
-	// Bookmark needs tag id, timestamp
-	/*private static Map<Integer, Double> addActValue(Bookmark data, Map<Integer, Double> actValues, long baselineTimestamp, double dVal) {
-		if (!data.getTimestamp().isEmpty()) {
-			//calculates recency
-			Double recency = (double)(baselineTimestamp - Long.parseLong(data.getTimestamp()) + 1.0);
-			//double recency = Math.ceil((baselineTimestamp - Long.parseLong(data.getTimestamp()) + 1.0) / 60.0 / 60.0 / 24.0 / 365.0 / 10);
-			//System.out.println(recency);			
-			Double newAct = Math.pow(recency, dVal * -1.0);
-			
-			for (Integer value : data.getTags()) {
-				Double oldAct = actValues.get(value);
-				if (!newAct.isInfinite() && !newAct.isNaN()) {
-					actValues.put(value, (oldAct != null ? oldAct + newAct : newAct));
-				} else {
-					System.out.println(data.getUserID() + "_" + baselineTimestamp + " " + data.getTimestamp());
-				}
-			}
+
+	private Map<String, Double> calculateNormalizedActValue(Map<String, Long> userTags) {
+		double dVal = 0.5;
+		long currentTime = System.currentTimeMillis();
+		Map<String, Double> actValues = new LinkedHashMap<String, Double>();
+
+		for (Map.Entry<String, Long>tag : userTags.entrySet()){
+			Double recency = (double)(currentTime - tag.getValue() + 1.0);
+			double newAct = Math.pow(recency, dVal * -1.0);
+			double oldAct = 0.0;
+			if (actValues.containsKey(tag.getKey()))
+				oldAct = actValues.get(tag.getKey());
+			actValues.put(tag.getKey(), oldAct + newAct);
+		}
+
+		//normalize
+		double sum =0;
+		for (Map.Entry<String, Double> entry : actValues.entrySet()) {
+				sum += entry.getValue();
+		}
+		for (Map.Entry<String, Double> entry : actValues.entrySet()) {
+			entry.setValue(entry.getValue()/ sum);
 		}
 		return actValues;
-	}*/
+	}
+
+
+	private Map<String, Double> calculateNormalizedMostPopular(
+			Map<String, Integer> tagsByGroup) {
+
+		Map<String, Double> sortedMostPopular = new LinkedHashMap<String, Double>(tagsByGroup.size());
+		MPValueComparator mpvc = new MPValueComparator(tagsByGroup);
+		TreeMap<String, Integer> sorted_map = new TreeMap<String, Integer>(mpvc); 
+		sorted_map.putAll(tagsByGroup);
+		
+		// TODO: normalize values - this is one approach CHECK!!!
+		// TODO: check whether List is sorted correctly!!
+		//normalize
+		double sum =0;
+		for (Map.Entry<String, Integer> entry : sorted_map.entrySet()) {
+				sum += entry.getValue();
+		}
+		for (Map.Entry<String, Integer> entry : sorted_map.entrySet()) {
+			sortedMostPopular.put(entry.getKey(), entry.getValue()/ sum);
+		}
+		return sortedMostPopular;
+	}
+}
+
+
+class MPValueComparator implements Comparator<String> {
+	
+    Map<String, Integer> base;
+    public MPValueComparator(Map<String, Integer> base) {
+        this.base = base;
+    }
+
+    // Note: this comparator imposes orderings that are inconsistent with equals.    
+    public int compare(String a, String b) {
+        if (base.get(a) >= base.get(b)) {
+            return -1;
+        } else {
+            return 1;
+        } // returning 0 would merge keys
+    }
+}
+
+
+class ActValueComparator implements Comparator<String> {
+	
+    Map<String, Double> base;
+    
+    public ActValueComparator(Map<String, Double> base) {
+        this.base = base;
+    }
+
+    // Note: this comparator imposes orderings that are inconsistent with equals.    
+    public int compare(String a, String b) {
+        if (base.get(a) >= base.get(b)) {
+            return -1;
+        } else {
+            return 1;
+        } // returning 0 would merge keys
+    }
 }
